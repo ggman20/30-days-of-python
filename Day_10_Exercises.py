a9d9d8a5ab1962eb17cfd21020f7dafe81af67c2
Gokhan <gokhanarman89@gmail.com>
2022-04-20 10:48:38 +0300

    Day 10 is started

---
+++ Day_10_Exercises.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 09:35:36 2022
+
+@author: ARMAN
+"""
+#%% LEVEL - 1
+# 1. Iterate 0 to 10 using for loop, do the same using while loop.
+
+# 2. Iterate 10 to 0 using for loop, do the same using while loop.
+
+# 3. Write a loop that makes seven calls to print(), so we get on the output the following triangle:
+    
+
+#%% LEVEL - 2 
